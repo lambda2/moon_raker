3887d7e7be6e280c574fc635586de67f5628ce12
Andre Aubin <andre@orga.42.fr>
2015-09-02 19:03:26 +0200

    assets

--- app/public/apipie/javascripts/apipie.js
+++ app/public/apipie/javascripts/apipie.js
@@ -3,4 +3,16 @@ $(document).ready(function() {
     $('pre.ruby').addClass('prettyprint lang-rb');
     prettyPrint();
   }
+
+  if ($("#page-menu").length > 0) {
+    var active = $("body").attr("data-active")
+    var menu = $("<ul id='local-menu' class='nav' role='tablist'></ul>")
+
+    $("h2[id]").each(function(){
+      menu.append("<li><a href='#" + $(this).attr("id") + "'>" + $(this).html() + "</a></li>")
+    });
+
+    $("#page-menu li.link-to-" + active).after(menu)
+    $('body').scrollspy({ target: '#page-menu' })
+  }
 });
